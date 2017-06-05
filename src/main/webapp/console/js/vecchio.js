2face976fcadb94c8f3f86a8a2ccda68b33ebec0
Alexandre Oliveira <alexandre.oliveira@sumware.com.br>
2017-06-05 20:19:56 -0300

    - ui.router

--- src/main/webapp/console/js/vecchio.js
+++ src/main/webapp/console/js/vecchio.js
@@ -1,4 +1,32 @@
-var vecchioApp = angular.module('vecchioApp', []);
+var vecchioApp = angular.module('vecchioApp', [ 'ui.router' ]);
+
+vecchioApp.config(function($stateProvider, $urlRouterProvider) {
+
+	$urlRouterProvider.otherwise('/home/config');
+	$stateProvider.state('home', {
+		url : '/home',
+		templateUrl : 'home.html'
+	}).state(
+			'home.configuration',
+			{
+				url : '/config',
+				templateUrl : 'configuration.html',
+				controller : function($scope, $http, $window) {
+					$scope.mappings = null;
+
+					$scope.$evalAsync($http.get(
+							"http://localhost:8080/api/mapping/").then(
+							function(response) {
+								$scope.mappings = response.data;
+							}));
+				}
+			})
+
+	.state('about', {
+	// we'll get to this in a bit
+	});
+
+});
 
 vecchioApp.controller('VecMappingCtrl', [
 		"$scope",
